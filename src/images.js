b17444f700f58cbdf98d01c12f819ba53a0d00e5
segunojo1 <olusegunkd2017@gmail.com>
2023-09-21 14:53:36 +0100

    img issues

--- src/images.js
+++ src/images.js
@@ -1,51 +1,51 @@
 export const images = [
         {
-                "url": "/src/assets/1.jpg",
+                "url": "/assets/1.jpg",
                 "id" : "1",
                 "tags": "mountain"
               },
               {
-                "url": "/src/assets/2.jpg",
+                "url": "//assets/2.jpg",
                 "id" : "2",
                 "tags": "waterfall"
               },
               {
-                "url": "/src/assets/3.jpg",
+                "url": "//assets/3.jpg",
                 "id" : "3",
                 "tags": "tree"
               },
               {
-                "url": "/src/assets/4.jpg",
+                "url": "//assets/4.jpg",
                 "id" : "4",
                 "tags": "landscape"
               },
               {
-                "url": "/src/assets/5.jpg",
+                "url": "//assets/5.jpg",
                 "id" : "5",
                 "tags": "mountain"
               },
               {
-                "url": "/src/assets/6.jpg",
+                "url": "//assets/6.jpg",
                 "id" : "6",
                 "tags": "landscape"
               },
               {
-                "url": "/src/assets/7.jpg",
+                "url": "//assets/7.jpg",
                 "id" : "7",
                 "tags": "flower"
               },
               {
-                "url": "src/assets/8.jpg",
+                "url": "/assets/8.jpg",
                 "id" : "8",
                 "tags": "sunset"
               },
               {
-                "url": "src/assets/9.jpg",
+                "url": "/assets/9.jpg",
                 "id" : "9",
                 "tags": "hill"
               },
               {
-                "url": "src/assets/10.jpg",
+                "url": "/assets/10.jpg",
                 "id" : "10",
                 "tags": "ocean"
               }
