c2f7fa39d7300e12b178ee98e79742530388ec7f
segunojo1 <olusegunkd2017@gmail.com>
2023-09-21 15:09:36 +0100

    change imgs to svg

--- src/images.js
+++ src/images.js
@@ -10,42 +10,42 @@ export const images = [
                 "tags": "waterfall"
               },
               {
-                "url": "/3.jpg",
+                "url": "/3.svg",
                 "id" : "3",
                 "tags": "tree"
               },
               {
-                "url": "/4.jpg",
+                "url": "/4.svg",
                 "id" : "4",
                 "tags": "landscape"
               },
               {
-                "url": "/5.jpg",
+                "url": "/5.svg",
                 "id" : "5",
                 "tags": "mountain"
               },
               {
-                "url": "/6.jpg",
+                "url": "/6.svg",
                 "id" : "6",
                 "tags": "landscape"
               },
               {
-                "url": "/7.jpg",
+                "url": "/7.svg",
                 "id" : "7",
                 "tags": "flower"
               },
               {
-                "url": "/8.jpg",
+                "url": "/8.svg",
                 "id" : "8",
                 "tags": "sunset"
               },
               {
-                "url": "/9.jpg",
+                "url": "/9.svg",
                 "id" : "9",
                 "tags": "hill"
               },
               {
-                "url": "/10.jpg",
+                "url": "/10.svg",
                 "id" : "10",
                 "tags": "ocean"
               }
